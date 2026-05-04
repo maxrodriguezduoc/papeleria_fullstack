1a02edc249c5a3eb3ee8de3215afe48804d0c7de
maxrodriguezduoc <max.rodriguez@duocuc.cl>
2026-05-04 18:40:58 -0400

    Add files via upload

---
+++ papeleria/src/test/java/com/theoffice/papeleria/PapeleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theoffice.papeleria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapeleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
